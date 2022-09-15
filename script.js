86667bcdb116f8cbd92f2640b4aae0f245ad3eee
MurgrayDJ <murgraydj@gmail.com>
2022-09-15 15:35:00 -0400

    Changes the name "grid cube" to "grid square"

--- script.js
+++ script.js
@@ -2,19 +2,21 @@
 //Date: 9/14/2022
 //Purpose: To emulate a sketchpad/Etch-a-Sketch
 
-
+//Creates a grid container, then creates several grid squares to add to it.
+//Then appends the container with the squares inside to the body of the DOM
 function createGrid(){
     const gridContainer = document.createElement('div');
     gridContainer.classList.add('grid-container');
 
     for(let i = 0; i < 16; i++){
-        const gridCube = document.createElement('div');
-        gridCube.classList.add('grid-cube');
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
 
-        gridContainer.appendChild(gridCube);
+        gridContainer.appendChild(gridSquare);
     }
 
     document.body.appendChild(gridContainer);
 }
 
 createGrid();
+
