67420ec022a719df2f2c9121a08563fd8b245fc8
MurgrayDJ <murgraydj@gmail.com>
2022-09-15 14:28:17 -0400

    Adds grid container and squares via script.js
    Instead of appending each grid-square to the grid container div already
    in the html, program now creates the grid container in javascript,
    appends each square to the container, and then adds the entire container
    containing the squares to the DOM at once.

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 //Date: 9/14/2022
 //Purpose: To emulate a sketchpad/Etch-a-Sketch
 
+
 function createGrid(){
-    const gridContainer = document.querySelector(".grid-container");
+    const gridContainer = document.createElement('div');
+    gridContainer.classList.add('grid-container');
 
     for(let i = 0; i < 16; i++){
         const gridCube = document.createElement('div');
         gridCube.classList.add('grid-cube');
 
-        gridContainer.appendChild('gridCube');
+        gridContainer.appendChild(gridCube);
     }
+
+    document.body.appendChild(gridContainer);
 }
 
 createGrid();
