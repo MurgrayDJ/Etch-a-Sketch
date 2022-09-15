118f27616dd17665ca0facf007c2fea34d2bfda3
MurgrayDJ <murgraydj@gmail.com>
2022-09-15 14:04:11 -0400

    Main heading and grid container centered on page
    The heading "Etch-a-Sketch" Appears centered towards the top of the
    page. The grid container that will contain a 16 x 16 grid also appears
    under the heading, with a solid, black line.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 //Author: Murgray D. John
 //Date: 9/14/2022
 //Purpose: To emulate a sketchpad/Etch-a-Sketch
+
+function createGrid(){
+    const gridContainer = document.querySelector(".grid-container");
+
+    for(let i = 0; i < 16; i++){
+        const gridCube = document.createElement('div');
+        gridCube.classList.add('grid-cube');
+
+        gridContainer.appendChild('gridCube');
+    }
+}
+
+createGrid();
